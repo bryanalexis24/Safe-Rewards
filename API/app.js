b85504411274d15fd17fb46cce6f0710ba29f784
sayada <sayadaaro@gmail.com>
2024-05-16 19:39:41 -0400

    files for the backend, scheduler (not done)

---
+++ API/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const app = express();
+
+const port = 3000;
+
+app.use(express.urlencoded({extended:true}));
+
+app.use(express.json());
+
+const user_controller = require("../API/Controllers/user_controller");
+app.use('/users', user_controller);
+app.get('/', (req, res) => {
+
+    res.send("This is working");
+
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
+
+  module.exports = app;
+
